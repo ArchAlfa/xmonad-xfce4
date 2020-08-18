ab0fa5b5ebd64c3d109b887f7002bd156647b091
ArchAlfa <68614724+ArchAlfa@users.noreply.github.com>
2020-08-18 12:42:50 +0200

    Update Layouts.hs

--- lib/modules/Layouts.hs
+++ lib/modules/Layouts.hs
@@ -6,28 +6,37 @@ module Modules.Layouts where
 
 -- MODULES
 import XMonad
+import qualified Modules.Variables as V
 
+-- HOOKS
 import XMonad.Hooks.ManageDocks
 
+-- LAYOUTS
+
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Simplest
+import XMonad.Layout.GridVariants
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spiral
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ThreeColumns
+
+-- LAYOUT MODIFIERS
+
+-- import XMonad.Layout.ShowWName
+-- import qualified XMonad.Layout.Gaps as G
+import XMonad.Layout.WindowArranger
+import qualified XMonad.Layout.ToggleLayouts as T
+import qualified XMonad.Layout.MultiToggle as MT
+import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed
 import XMonad.Layout.Decoration
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LimitWindows
 import XMonad.Layout.Magnifier
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Renamed
-import XMonad.Layout.Spacing
-import XMonad.Layout.Tabbed
-import XMonad.Layout.WindowArranger
-import qualified XMonad.Layout.ToggleLayouts as T
-import qualified XMonad.Layout.MultiToggle as MT
-
-import XMonad.Layout.GridVariants
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.Spiral
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.ThreeColumns
 
 -- LIST -----------------------------------------------------------------------
 
@@ -40,7 +49,6 @@ magnify =
     renamed [Replace "magnify"]
     $ magnifier
     $ limitWindows 12
-    -- $ mySpacing 0
     $ ResizableTall 1 (3/100) (1/2) []
 
 monocle =
@@ -50,38 +58,29 @@ monocle =
 grid =
     renamed [Replace "grid"]
     $ limitWindows 12
-    -- $ mySpacing 8
     $ mkToggle (single MIRROR)
     $ Grid (16/10)
 
 spirals =
     renamed [Replace "spirals"]
-    -- $ mySpacing' 0
     $ spiral (6/7)
 
 threeCol =
     renamed [Replace "threeCol"]
     $ limitWindows 7
-    -- $ mySpacing' 8
     $ ThreeCol 1 (3/100) (1/2)
 
 threeRow =
     renamed [Replace "threeRow"]
     $ limitWindows 7
-    -- $ mySpacing' 8
     $ Mirror
     $ ThreeCol 1 (3/100) (1/2)
 
+
 -- CONFIG ---------------------------------------------------------------------
 
-myLayout =
-    avoidStruts $ windowArrange $mkToggle (NBFULL ?? NOBORDERS ?? EOT)
+myLayout = avoidStruts
+    $ windowArrange 
+    $ mkToggle (NBFULL ?? NOBORDERS ?? EOT)
     $ myDefaultLayout
-    
-    where
-    myDefaultLayout =
-        tall
-        ||| Full
-        ||| threeCol
-        ||| threeRow
-        ||| grid
+    where myDefaultLayout = tall ||| Full ||| threeCol ||| threeRow ||| grid
